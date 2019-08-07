5795c0d825e0f09d784b50851828e90504a17a7a
Sachin Tewari <xachin@gmail.com>
2019-08-07 22:08:46 +0800

    Initial commit

---
+++ src/jvmMain/kotlin/sample/SampleJvm.kt
@@ -0,0 +1,13 @@
+package sample
+
+actual class Sample {
+    actual fun checkMe() = 42
+}
+
+actual object Platform {
+    actual val name: String = "JVM"
+}
+
+fun main() {
+    println(hello())
+}
